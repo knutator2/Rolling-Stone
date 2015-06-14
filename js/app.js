c5f3c8caaddeb335a3d85cf8cc1d88f4d288dfbc
Knut Perseke <knut.perseke@gmail.com>
2015-06-14 21:22:23 +0200

    fixed bug for loading module

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 var myApp = angular.module('RollingStone', ['leaflet-directive', 'stoneparsingservice']);
+
+// myApp.config(function($routeProvider) {
+// 	$routeProvider
+// 		.when('/', {
+// 			templateUrl: 'map.html'
+// 			controller: 'MapController'
+// 		})
+// });
