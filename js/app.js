ab13f9a13806baf03abf384ac0b6532b3ba27d54
Knut Perseke <knut.perseke@gmail.com>
2015-06-15 18:26:12 +0200

    build app routing for different views and moved index content to map view

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
-var myApp = angular.module('RollingStone', ['leaflet-directive', 'stoneparsingservice']);
+var myApp = angular.module('RollingStone', ['leaflet-directive', 'stoneparsingservice', 'ngRoute', 'ngAnimate']);
 
-// myApp.config(function($routeProvider) {
-// 	$routeProvider
-// 		.when('/', {
-// 			templateUrl: 'map.html'
-// 			controller: 'MapController'
-// 		})
-// });
+myApp.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'map.html',
+			controller: 'MapController'
+		})
+		.when('/map', {
+			templateUrl: 'map.html',
+			controller: 'MapController'
+		})
+		.when('/graph', {
+			templateUrl: 'graph.html',
+			controller: 'GraphController'
+		});
+});
