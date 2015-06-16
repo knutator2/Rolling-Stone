2cf3d1404faa835c36df8f1e9d86030cd0df704b
Knut Perseke <knut.perseke@gmail.com>
2015-06-16 23:01:52 +0200

    parse current stone from get parameter

--- js/controllers/GraphController.js
+++ js/controllers/GraphController.js
@@ -2,9 +2,15 @@ myApp.controller("GraphController", ['$scope', '$routeParams' ,'StonesService',
 	function($scope, $routeParams, StonesService) {
 
 		$scope.name = "Graph";
+		$scope.currentStone = {};
 
 		StonesService.get().then(function(data) {
 			$scope.len = data.length;
+			angular.forEach(data, function(value) {
+				if (value.museum_id === parseInt($routeParams.stone)) {
+					$scope.currentStone = value;
+				}
+			});
 			console.log($routeParams);
 		}); 
 	}
