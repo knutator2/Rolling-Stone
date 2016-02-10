159cd364f3c33579182e267e0966c2fb302ad629
Thomas Fett <git@thomasfett.de>
2016-02-10 23:20:28 +0100

    Amend - committed some errors before...

--- js/directives/timeline.js
+++ js/directives/timeline.js
@@ -1,4 +1,5 @@
 var $ = require( 'jquery' );
+var _ = require( 'underscore' );
 
 var timeline = function( StoneParsingService, StoneEpocheService ) {
     return {
@@ -46,58 +47,58 @@ var timeline = function( StoneParsingService, StoneEpocheService ) {
                 console.log(ticks);
 
 
-                var slider = $('#ex1').slider({
-                formatter: function(value) {
-                    return 'Current value: ' + value;
-                },
+                // var slider = $('#ex1').slider({
+                // formatter: function(value) {
+                //     return 'Current value: ' + value;
+                // },
                 //min: 0,
                 //max: 5,
-                step: 0.01,
-                value: [0,10],
-                //handle: "circle",
-                tooltip: 'hide',
-                ticks: ticks,
-                ticks_labels: tickLabels,
-                scale: 'linear',
+                // step: 0.01,
+                // value: [0,10],
+                // //handle: "circle",
+                // tooltip: 'hide',
+                // ticks: ticks,
+                // ticks_labels: tickLabels,
+                // scale: 'linear',
                 //reversed : true
             });
 
-            $("#ex1").on("slideStop", function(slideEvt) {
-                console.log(slideEvt.value);
-                var leftIndex = Math.round(slideEvt.value[0]);
-                var rightIndex = Math.round(slideEvt.value[1]);
-                slider.slider('setValue', [leftIndex, rightIndex]);
-                var leftEpoche = scope.epoches[leftIndex];
-                var rightEpoche = scope.epoches[rightIndex];
-                if (scope.stones.length > 0) {
-                    var stones_copy = JSON.parse(JSON.stringify(all_stones))
-                    var filtered_stones = _.map(stones_copy, function (elem) {
-                        elem.stones = _.filter(elem.stones, function (stone) {
-                            //var date = parseInt(stone.date_in_mya.split("-")[0]);
-                            //return date <= leftEpoche.start && date >= rightEpoche.end;
-                            for (var i = leftIndex; i <= rightIndex; i++) {
-                                if (stone.geological_era === scope.epoches[i].name) {
-                                    return true;
-                                }
-                            }
-                            return false;
-                        });
-                        return elem;
-                    });
-                    filtered_stones = _.filter(filtered_stones, function (elem) {
-                        return elem.stones.length > 0
-                    });
-                    console.log("want to update markers")
-                    scope.stones = filtered_stones;
-                }
-
-                //if (slideEvt.value[0] < 1)
-                //    scope.stones = all_stones;
-                // else {
-                //     scope.stones = [];
-                // }
-            });
-            });
+            // $("#ex1").on("slideStop", function(slideEvt) {
+            //     console.log(slideEvt.value);
+            //     var leftIndex = Math.round(slideEvt.value[0]);
+            //     var rightIndex = Math.round(slideEvt.value[1]);
+            //     // slider.slider('setValue', [leftIndex, rightIndex]);
+            //     var leftEpoche = scope.epoches[leftIndex];
+            //     var rightEpoche = scope.epoches[rightIndex];
+            //     if (scope.stones.length > 0) {
+            //         var stones_copy = JSON.parse(JSON.stringify(all_stones))
+            //         var filtered_stones = _.map(stones_copy, function (elem) {
+            //             elem.stones = _.filter(elem.stones, function (stone) {
+            //                 //var date = parseInt(stone.date_in_mya.split("-")[0]);
+            //                 //return date <= leftEpoche.start && date >= rightEpoche.end;
+            //                 for (var i = leftIndex; i <= rightIndex; i++) {
+            //                     if (stone.geological_era === scope.epoches[i].name) {
+            //                         return true;
+            //                     }
+            //                 }
+            //                 return false;
+            //             });
+            //             return elem;
+            //         });
+            //         filtered_stones = _.filter(filtered_stones, function (elem) {
+            //             return elem.stones.length > 0
+            //         });
+            //         console.log("want to update markers")
+            //         scope.stones = filtered_stones;
+            //     }
+            //
+            //     //if (slideEvt.value[0] < 1)
+            //     //    scope.stones = all_stones;
+            //     // else {
+            //     //     scope.stones = [];
+            //     // }
+            // });
+            // });
 
 
 
