7e78916ee1234eb665cb33be22bd91642b25b35d
Thomas Fett <git@thomasfett.de>
2017-05-20 23:35:13 +0200

    VUE-REFACTOR: Basic integration of vue-router.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,32 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 
+// HOT MODULE RELOADING
 if (module.hot) {
   module.hot.accept()
 }
 
-Vue.component('hello', require('./components/Hello.vue'));
+// COMPONENTS
+Vue.component('hello', require('./components/Hello.vue'))
+Vue.component('gallery-page', require('./components/Gallery/GalleryPage.vue'))
+Vue.component('map-page', require('./components/Map/MapPage.vue'))
+
+// ROUTER
+Vue.use(VueRouter)
+
+const routes = [
+    {path: '/', component: require('./components/Gallery/GalleryPage.vue')},
+    {path: '/gallery', component: require('./components/Gallery/GalleryPage.vue')},
+    {path: '/map', component: require('./components/Map/MapPage.vue')}
+]
+
+const router = new VueRouter({
+    routes
+})
 
 var app = new Vue({
   el: '#app',
+  router,
   data: {
     message: 'Hello Vue!'
   }
---
+++ src/components/Gallery/GalleryPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        This is the Gallery Page.
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
